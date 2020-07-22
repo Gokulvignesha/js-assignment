61debfd3480db284e3cef5629808523be8608df2
Gokul vignesha <53517650+Gokulvignesha@users.noreply.github.com>
2020-07-22 12:39:00 +0530

    Day 6 Assignments

---
+++ Day 6/Question 3/greet.js
@@ -0,0 +1,6 @@
+var name=prompt("Enter Your Name");
+var doc=document.getElementById("head");
+doc.innerText=`Hello, ${name} Welcome to our Page `;
+function myfunction(){
+    document.body.style.backgroundColor="black";
+}
